67eec759595523e27606ff58eb8989f7b49ae31c
tav <tav@espians.com>
2011-10-07 13:54:48 +0100

    Updated S3 sync script to handle directories.

--- sync.sh
+++ sync.sh
@@ -4,10 +4,20 @@
 # See the Pivotal Web UNLICENSE file for details.
 
 rm -rf gzipped
-mkdir -p gzipped
+mkdir -p gzipped/css
+mkdir -p gzipped/gfx
+mkdir -p gzipped/js
 
-for i in `ls static`; do
-	gzip <static/$i >gzipped/$i
+for i in `ls static/css`; do
+	gzip <static/css/$i >gzipped/css/$i
+done
+
+for i in `ls static/gfx`; do
+	gzip <static/gfx/$i >gzipped/gfx/$i
+done
+
+for i in `ls static/js`; do
+	gzip <static/js/$i >gzipped/js/$i
 done
 
 s3cmd -v -P -M --add-header "Content-Encoding: gzip" sync --delete-removed gzipped/ s3://$S3_BUCKET/
