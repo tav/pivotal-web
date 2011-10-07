3aa13d68a91348eac628b52819781822cd2a604c
tav <tav@espians.com>
2011-10-07 08:13:05 +0100

    Made an example config.py module.

---
+++ config.py
@@ -0,0 +1,13 @@
+# Public Domain (-) 2011 The Pivotal Web Authors.
+# See the Pivotal Web UNLICENSE file for details.
+
+AUTHORS = {
+    "Name": "their@email.address",
+}
+
+DEBUG = False
+LOCAL_DIRECTORY = False
+PROJECT_ID = "The Pivotal Tracker Project ID"
+PROJECT_TITLE = "Your Project Title"
+TRACKER_TOKEN = "Your Pivotal Tracker API Token"
+STATIC_URL_BASE = "Base URL for your static files when deployed"
