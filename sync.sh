7475472e8f3260cfc70420769e3c8face5b4d61b
tav <tav@espians.com>
2011-10-07 14:19:54 +0100

    Skipped gzipping the gfx assets.

--- sync.sh
+++ sync.sh
@@ -5,19 +5,16 @@
 
 rm -rf gzipped
 mkdir -p gzipped/css
-mkdir -p gzipped/gfx
 mkdir -p gzipped/js
 
 for i in `ls static/css`; do
 	gzip <static/css/$i >gzipped/css/$i
 done
 
-for i in `ls static/gfx`; do
-	gzip <static/gfx/$i >gzipped/gfx/$i
-done
-
 for i in `ls static/js`; do
 	gzip <static/js/$i >gzipped/js/$i
 done
 
-s3cmd -v -P -M --add-header "Content-Encoding: gzip" sync --delete-removed gzipped/ s3://$S3_BUCKET/
+s3cmd -v -P -M --add-header "Content-Encoding: gzip" sync --delete-removed gzipped/css/ s3://$S3_BUCKET/css/
+s3cmd -v -P -M --add-header "Content-Encoding: gzip" sync --delete-removed gzipped/js/ s3://$S3_BUCKET/js/
+s3cmd -v -P -M sync --delete-removed static/gfx/ s3://$S3_BUCKET/gfx/
